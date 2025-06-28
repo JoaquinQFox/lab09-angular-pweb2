a3a1ab32b831a15d065ce39057a8720861186ad0
romich1307 <rcamargoh@unsa.edu.pe>
2025-06-28 14:57:10 -0500

    feat: agregar ruta resultado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ResultadoComponent } from './resultado/resultado.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'resultado', component: ResultadoComponent },
+    { path: '', redirectTo: 'resultado', pathMatch: 'full' }
+];
