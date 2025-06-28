04a894366d05c38f517b9783a1fb0fca6e1a7c70
Paola0601 <pmaytaqu@unsa.edu.pe>
2025-06-28 18:18:20 -0500

    feat:Se crea la  logica basica para el juego

--- src/app/logica-ahorcado/logica-ahorcado.ts
+++ src/app/logica-ahorcado/logica-ahorcado.ts
@@ -1,5 +1,5 @@
 import { Component,OnInit } from '@angular/core';
-import { WordService} from '../word.service';
+import { Word} from '../word';
 
 @Component({
   selector: 'app-logica-ahorcado',
@@ -13,4 +13,18 @@ export class LogicaAhorcado  implements OnInit {
   letrasAdivinadas: string[] = [];
   intentos:number = 6;
   estadoJuego:boolean=false;
+
+  constructor(private wordService: Word) { }
+
+  ngOnInit(): void {
+    this.iniciarJuego();
+  }
+
+  iniciarJuego(): void {
+    this.palabra = this.wordService.getRandomWord();
+    this.letrasAdivinadas = [];
+    this.intentos = 6;
+    this.estadoJuego = false;
+    this.letrasNoAdivinadas = '_ '.repeat(this.palabra.length).trim();
+  }
 }
