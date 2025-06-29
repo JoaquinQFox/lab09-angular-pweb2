17f8844bcd9d84ce5da4a668e161b6f1c2e89f66
JoaquinQFox <jquispebed@unsa.edu.pe>
2025-06-29 17:46:17 -0500

    feat: se aumento la cantidad de palabras disponibles a adivinar

--- src/app/ahorcado/services/word.ts
+++ src/app/ahorcado/services/word.ts
@@ -5,7 +5,22 @@ import { Injectable } from '@angular/core';
 })
 export class Word {
 
-  private words = ['Amor', 'Cabeza', 'Chocolate', 'Papa'];
+  private words = [
+    'Amigos', 'Montana', 'Ventana', 'Camisa', 'Sombrero', 'Zapato', 'Pelota', 'Escoba',
+    'Carroza', 'Callejon', 'Frutero', 'Espejo', 'Silla', 'Cuchara', 'Tenedor', 'Viento',
+    'Bailar', 'Caballo', 'Caminar', 'Pintura', 'Libro', 'Librero', 'Florero', 'Telefono',
+    'Escuela', 'Montaje', 'Helados', 'Pastel', 'Galleta', 'Cocina', 'Paredes', 'Pintor',
+    'Escritor', 'Maestro', 'Carpeta', 'Cartera', 'Computadora', 'Teclado', 'Pantalla',
+    'Ratones', 'Animal', 'Plantas', 'Cactus', 'Reptil', 'Cabello', 'Limpieza', 'Aventura',
+    'Cochera', 'Lampara', 'Zapallo', 'Bananas', 'Sandias', 'Tomates', 'Pescado', 'Camaron',
+    'Girasol', 'Papelon', 'Cartuchera', 'Maletas', 'Cinturon', 'Carteles', 'Cuaderno',
+    'Lapices', 'Colores', 'Balones', 'Futbol', 'Basquet', 'Bicicleta', 'Tractor', 'Aviones',
+    'Barcos', 'Cielito', 'Globito', 'Sombras', 'Montanas', 'Colinas', 'Pradera', 'Desierto',
+    'Bosques', 'Islotes', 'Castillo', 'Tormenta', 'Niebla', 'Temblor', 'Tsunami', 'Huracan',
+    'Estrella', 'Cometas', 'Planeta', 'Galaxia', 'Universo', 'Escalera', 'Ventilador',
+    'Motoras', 'Tornillos', 'Martillo', 'Agujero'
+  ];
+
   constructor() { }
 
   getRandomWord(): string {
