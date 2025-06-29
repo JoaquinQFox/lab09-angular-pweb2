81f3e898b09ca9bbacac81aa8e6917a2438ea1b7
JoaquinQFox <jquispebed@unsa.edu.pe>
2025-06-28 22:05:54 -0500

    feat: se agrega redirección de página home a página de juego

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { Navbar } from '../shared/navbar/navbar';
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar],
+  imports: [RouterLink, Navbar],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
