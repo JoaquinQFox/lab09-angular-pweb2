e93e833cf079bf595d23bf43b670c11b06873726
Paola0601 <pmaytaqu@unsa.edu.pe>
2025-06-29 16:22:31 -0500

    feat:Se crea un compente del footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './shared/navbar/navbar';
+import { FooterComponent } from './shared/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
